72c700fe2329cbe6bf24ec4428f29f04d4958af3
adele tafazoli <adeletfz@gmail.com>
2020-06-08 01:04:56 +0430

    first changes

---
+++ src/components/HomePageComponents/workshops/workshops.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Paint from '../../../constants/images/paint.png'
+
+const Workshops:React.FC=()=>{
+return(
+    <div>
+        {Paint}
+    </div>
+)
+}
+export default Workshops
---
+++ src/components/exercise/index.tsx
@@ -0,0 +1,2 @@
+import React from 'react'
+import Paint from '../../constants/images/paint.png'
