d208e116c369988e77787701a710516d4fec9765
adele tafazoli <adeletfz@gmail.com>
2020-06-23 21:59:46 +0430

    stylish statics

--- src/components/Statics/statics.tsx
+++ src/components/Statics/statics.tsx
@@ -59,7 +59,9 @@ const Statics:React.FC =()=>{
 
 
     return(
-      <div style={{width:'100%',height:415,marginTop:100}}>
+      <div style={{width:'100%',marginTop:100}}>
+        <svg style={{marginBottom:'-28%',position:'relative',zIndex:-1}}xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00d" fill-opacity="1" d="M0,224L80,208C160,192,320,160,480,128C640,96,800,64,960,90.7C1120,117,1280,203,1360,245.3L1440,288L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+        <svg style={{marginBottom:'-3%'}}xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00B8D4" fill-opacity="1" d="M0,224L80,213.3C160,203,320,181,480,192C640,203,800,245,960,256C1120,267,1280,245,1360,234.7L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
         <div style={{width:"100%", color:"#fff",height:'300px',backgroundColor:'#00B8D4', display:'flex',alignItems:'center', justifyContent:"space-around"}}>
 <div style={{display:'flex', flexDirection:"column"}}>
     <div style={{textAlign:'center',fontSize:60}}>{teacher}</div>
@@ -74,9 +76,14 @@ const Statics:React.FC =()=>{
     <p style={{textAlign:'center',fontSize:25}}>years of experience</p>
     </div>
         </div>
-        
-        <svg style={{width:'100%' ,height:400,marginTop:-350 }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" 
-        fill-opacity="1" d="M0,224L120,234.7C240,245,480,267,720,277.3C960,288,1200,288,1320,288L1440,288L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
+        <svg style={{marginTop:-210 , position:'relative',}}xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path fill="#fff" fill-opacity="1" d="M0,224L80,202.7C160,181,320,139,480,138.7C640,
+          139,800,181,960,192C1120,203,1280,181,1360,170.7L1440,160L1440,320L1360,320C1280,320,
+          1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+          <div style={{height:150,overflow: 'hidden',marginTop:'-15%',position:'relative',zIndex:-1}} ><svg viewBox="0 0 500 150" 
+          preserveAspectRatio="none" style={{height: '100%', width: '100%'}}><path 
+          d="M-0.84,57.72 C349.60,277.78 349.20,-49.98 500.00,49.98 L500.27,-1.47 L-0.84,-3.45 Z" 
+          style={{stroke: 'none', fill: '#00d'}}></path></svg></div>
         </div>
     )
 }
